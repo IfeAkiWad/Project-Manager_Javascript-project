a10e6a5ae0da1d6939a503caaae74c51effb469a
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2020-12-09 14:05:22 -0600

    building projects

--- project_manager-backend/app/controllers/projects_controller.rb
+++ project_manager-backend/app/controllers/projects_controller.rb
@@ -4,4 +4,11 @@ class ProjectsController < ApplicationController
         render json: projects, except: [:created_at, :updated_at, :developer_id]
     end
 
+    def show
+        project = Project.find_by(id: params[:id])
+        # if params[:started] == " "
+        #     render json: { message: 'Start project' }
+        #   end
+    end
+
 end
