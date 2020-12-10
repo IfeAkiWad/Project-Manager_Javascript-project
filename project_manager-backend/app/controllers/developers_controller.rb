eeb0552a3486c6e606b442961ecb404cc778cf48
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2020-12-09 18:01:34 -0600

    incremental update

--- project_manager-backend/app/controllers/developers_controller.rb
+++ project_manager-backend/app/controllers/developers_controller.rb
@@ -1,6 +1,11 @@
 class DevelopersController < ApplicationController
-    # def index
-    #     developers = Developer.all
-    #     render json: developers
-    # end
+    def index
+        developers = Developer.all
+        render json: developers 
+    end
+    
+    def show
+            developer = Developer.find_by(id: params[:id])
+            render json: developer, include: [:projects]
+    end
 end
