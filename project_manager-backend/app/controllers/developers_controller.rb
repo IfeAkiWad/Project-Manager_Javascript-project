4f8a0cbd9c409b1553fa3b96ec12b9539315954c
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2020-12-09 19:37:03 -0600

    updating developers controller

--- project_manager-backend/app/controllers/developers_controller.rb
+++ project_manager-backend/app/controllers/developers_controller.rb
@@ -1,11 +1,11 @@
 class DevelopersController < ApplicationController
     def index
         developers = Developer.all
-        render json: developers, except: [:created_at, :updated_at]
+        render json: developers.to_json(except: [:created_at, :updated_at])
     end
     
     def show
-            developer = Developer.find_by(id: params[:id])
-            render json: developer.to_json(:include => {:projects => {:only => [:name, :started, :deadline, :description, :completed]}}, :except => [:updated_at], :except => [:created_at, :updated_at])
+        developer = Developer.find_by(id: params[:id])
+        render json: developer.to_json(:include => {:projects => {:only => [:name, :started, :deadline, :description, :completed]}}, :except => [:updated_at], :except => [:created_at, :updated_at])
     end
 end
