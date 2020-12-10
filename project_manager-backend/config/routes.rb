ab381c32a74ddbaa81683943eb2b4e815e80c24c
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2020-12-09 22:12:53 -0600

    updating routes

--- project_manager-backend/config/routes.rb
+++ project_manager-backend/config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   resources :projects, only: [:show, :index]
   resources :developers
 
-  root 'developers#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
