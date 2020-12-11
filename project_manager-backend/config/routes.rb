35de4551680d4c1f83af335d3bec6ad8f35c8797
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2020-12-11 00:20:16 -0600

    psterisk : config/initi/cors - origins

--- project_manager-backend/config/routes.rb
+++ project_manager-backend/config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :projects, only: [:show, :index]
   resources :developers
 
+  get '/test', to: 'application#test'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
