c36119651b1b58fe05cebb1c8906983eadb0fff6
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2020-12-10 23:38:37 -0600

    incremental update

--- project_manager-frontend/src/index.js
+++ project_manager-frontend/src/index.js
@@ -1,8 +1,7 @@
 // test that we can get data from the backend
-const BACKEND_URL = 'localhost:3000';
-fetch(`${BACKEND_URL}/test`)
+const BACKEND_URL = 'http://localhost:3000/developers/3';
+fetch(`${BACKEND_URL}`)
   .then(response => response.json())
   .then(parsedResponse => console.log(parsedResponse));
 
   console.log("testing...")
-  
