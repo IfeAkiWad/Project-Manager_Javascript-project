a52fca5cc3e583563c7400c7e0e7ae678d3a3742
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2020-12-11 17:32:04 -0600

    nested resource in routes

--- project_manager-backend/config/routes.rb
+++ project_manager-backend/config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  resources :projects, only: [:show, :index]
-  resources :developers
+  # resources :projects, only: [:show, :index]
+  # resources :developers
 
-  get '/test', to: 'application#test'
+  resources :developers do
+    # nested resource for reviews
+    resources :projects
+  end
+
+  # get '/test', to: 'application#test'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
