5f5597d84e5410e62c402d7ea912bfb45ff531e9
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2020-12-11 01:10:17 -0600

    connecting JS to Rails

--- project_manager-frontend/src/index.js
+++ project_manager-frontend/src/index.js
@@ -1,7 +1,7 @@
 // test that we can get data from the backend
-const api  = new ApiService('http://localhost:3000');
+
 // fetch(`${BACKEND_URL}/test`)
 //   .then(response => response.json())
 //   .then(parsedResponse => console.log(parsedResponse));
-
-  console.log('api')
+const api  = new ApiService('http://localhost:3000');
+console.log('api')
