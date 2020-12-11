6c85755b70da8e5ad7159596ca093516b2b99b1c
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2020-12-11 00:39:18 -0600

    mkdir service component touch ApiService

--- project_manager-frontend/src/index.js
+++ project_manager-frontend/src/index.js
@@ -1,6 +1,6 @@
 // test that we can get data from the backend
-const BACKEND_URL = 'http://localhost:3000/developers/3';
-fetch(`${BACKEND_URL}`)
+const BACKEND_URL = 'http://localhost:3000';
+fetch(`${BACKEND_URL}/test`)
   .then(response => response.json())
   .then(parsedResponse => console.log(parsedResponse));
 
