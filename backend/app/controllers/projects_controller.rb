d05122828f2250022bd2dc4819d5c0d9b8ddc6e8
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2021-01-05 10:45:55 -0600

    updates in projects controller

--- backend/app/controllers/projects_controller.rb
+++ backend/app/controllers/projects_controller.rb
@@ -1,62 +1,59 @@
 class ProjectsController < ApplicationController
-    # before_action :set_developer
     before_action :set_project, only: [:show, :edit, :update]
-    
-    # project is nil and doctor is nil
-
+       # project is nil and doctor is nil
+   
     def index
         @projects = Project.all
-        # binding.pry
-        # @projects = @developer.projects #causes error: undefined local variable or method `developer'
+            # binding.pry
+            # @projects = @developer.projects #causes error: undefined local variable or method `developer'
         render json: @projects.to_json(except: [:created_at, :updated_at])
-       
     end
-
+   
     def create
-        @project = Project.new(project_params)
-        # @project = @developer.projects.new(project_params) 
-        if @project.save
-            render_project       
-        else
-            render json: @project.errors, status: :unprocessable_entity
-        end
+        @project = Project.create(project_params)
+           # @project = @developer.projects.new(project_params) 
+            if @project.save
+               render_project       
+           else
+               render json: @project.errors, status: :unprocessable_entity
+           end
     end
-
+   
     def show
-        # project = Project.find_by(id: params[:id])
-        # if params[:started] == " "
-        #     render json: { message: 'Start project' }
-        #   end
+           # project = Project.find_by(id: params[:id])
+           # if params[:started] == " "
+           #     render json: { message: 'Start project' }
+           #   end
         render_project
     end
-
+   
     def edit
     end
-
+   
     def update # how do I build this using json ???
         if @project.update(project_params)
             render_project
         else
-            # ???
+               # ???
             render json: @project.errors, status: :unprocessable_entity        end
     end
-
+   
     private
-
-    def render_project
-        render json: @project.to_json(except: [:created_at, :updated_at])
-    end
-
-    # def set_developer
-    #     # @developer = Developer.find_by_id(params[:developer_id])
-    # end
-
-    def set_project
-        @project = Project.find_by_id(params[:id])
-    end
-
-    def project_params
-        params.require(:project).permit(:name, :started, :deadline, :description, :completed, :developer_id)
-    end
-
+   
+       def render_project
+           render json: @project.to_json(except: [:created_at, :updated_at])
+       end
+   
+       # def set_developer
+       #     # @developer = Developer.find_by_id(params[:developer_id])
+       # end
+   
+       def set_project
+           @project = Project.find_by_id(params[:id])
+       end
+   
+       def project_params
+           params.require(:project).permit(:name, :started, :deadline, :description, :completed)
+       end
 end
+   
