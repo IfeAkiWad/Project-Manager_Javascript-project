182f46a6c7ab6b0284b633aeacd3d6241f1f612f
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2021-03-14 22:14:32 -0500

    updated routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :projects
   
-  resources :developers do
+  resources :developers, only: [:index] do
     resources :projects
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
