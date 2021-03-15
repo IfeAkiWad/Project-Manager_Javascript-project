a19a197971c4fd99a3c3980815d1b6527c1f0684
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2021-03-14 22:15:19 -0500

    debugging edit form PATCH request

--- frontend/index.js
+++ frontend/index.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     Developer.getDeveloper()
     Projects.getAllProjects()
     newProjectForm()
-    editProject()
+    Projects.editProject()
 })
 
 // Below is code that handles all that relates to the project form.
@@ -23,7 +23,8 @@ function newProjectForm() {
     <label for="description">Project Description:</label><br><br>
     <textarea id="description"></textarea><br><br>
     <input type="submit" class="submit" value="New Project">
-    
+    <br><br>
+    <br><br>
     </form>
     `
     // add event listener 
@@ -67,64 +68,69 @@ function submitProjectForm(event) {
 }
 
 // edit button for each object
-function editProject() {
-    const projectParentNode = document.getElementById('projects-container')
-    projectParentNode.addEventListener('click', (event) => { //to access each project's edit button
-        console.log('is this working?')
-        console.log(event)
-        const editBtn = document.getElementById('edit')
-        if(editBtn) {
-            console.log('inside edit button')
-            let projectsContainer = document.getElementById("projects-container")  
-            projectsContainer.innerHTML +=     
-            `
-            <form id='form'>
-            <label for="name">Project Name:</label>
-            <input type="text" class="name" id="name"><br><br>
-            <label for="started">Project Started:</label>
-            <input type="date" class="started" id="started"><br><br>
-            <label for="deadline">Project Deadline:</label>
-            <input type="date" class="deadline" id="deadline"><br><br>
-            <label for="description">Project Description:</label><br><br>
-            <textarea id="description"></textarea><br><br>
-            <input type="submit" class="submit" value="Update Project">
+// function editProject() {
+//     const projectParentNode = document.getElementById('projects-container')
+//     projectParentNode.addEventListener('click', (event) => { //to access each project's edit button
+//         console.log('is this working?')
+//         console.log(event)
+//         const editBtn = document.getElementById('edit')
+//         if(editBtn) {
+//             console.log('inside edit button')
+//             let editProject = document.getElementById("form")
+//             editProject.innerHTML += // ERROR: Do not know how to clear form after appending.
+//                                           // ERROR: Do not even know if the project object can be updated yet 
+//             `
+//             <form id='edit-form'>
+//             <h2 id="form-header"> Edit project! </h2><br>
+//             <label for="name">Project Name:</label>
+//             <input type="text" class="name" id="name"><br><br>
+//             <label for="started">Project Started:</label>
+//             <input type="date" class="started" id="started"><br><br>
+//             <label for="deadline">Project Deadline:</label>
+//             <input type="date" class="deadline" id="deadline"><br><br>
+//             <label for="description">Project Description:</label><br><br>
+//             <textarea id="description"></textarea><br><br>
+//             <input type="submit" class="submit" data-id=value="Update Project">
             
-            </form>
-            `
-            projectsContainer.addEventListener("submit", updateProject)
-        }
-    })
-}
-// callback function for editProject()
-function updateProject() {
-    let name = document.getElementById("name").value
-    let started = document.getElementById("started").value
-    let deadline = document.getElementById("deadline").value
-    let description = document.getElementById("description").value
-    let userInput = {
-        name: name, 
-        started: started,
-        deadline: deadline,
-        description: description
-    };
-    fetch('http://localhost:3000/projects', { 
-            method: 'PATCH',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            },
-            body: JSON.stringify(userInput)
-        })
-        .then(response => response.json())
-        .then(project => {
-            const {id, name, started, deadline, description, developer_id} = project
-           let p = new Projects(id, name, started, deadline, description, developer_id)
-                p.renderProject()
-        })
+//             </form>
+//             `
+//             editProject.addEventListener("submit", updateProject(event))
+//         }
+//     })
+// }
+// // callback function for editProject()
+// function updateProject(event) {
+//     event.preventDefault()
+//     let name = document.getElementById("name").value
+//     let started = document.getElementById("started").value
+//     let deadline = document.getElementById("deadline").value
+//     let description = document.getElementById("description").value
+//     let userInput = {
+//         name: name, 
+//         started: started,
+//         deadline: deadline,
+//         description: description
+//     };
+//     fetch('http://localhost:3000/projects', { 
+//             method: 'PATCH',
+//             headers: {
+//                 'Content-Type': 'application/json',
+//                 // 'Accept': 'application/json'
+//             },
+//             body: JSON.stringify(userInput)
+//         })
+//         .then(response => response.json())
+//         .then(project => {
+//             const {id, name, started, deadline, description, developer_id} = project
+//            let p = new Projects(id, name, started, deadline, description, developer_id)
+//                 p.renderProject()
+//         })
     
-    
+//         // let updateProject = document.getElementById("project")
+//         // updateProject.reset()
 
-}
+
+// }
 // edit existinf project: overlay
 // Will need to append datasets ??? in order to give each project its own id ????? WTF
 // take a look at the Objects lesson
--- frontend/src/components/Projects.js
+++ frontend/src/components/Projects.js
@@ -6,6 +6,7 @@ class Projects {
         this.deadline = deadline
         this.description = description
         this.developer_Id = developer_Id
+        this.updateProject
     }
 
         // to get projects index
@@ -38,7 +39,7 @@ class Projects {
 
         projectsDiv.innerHTML +=
         `
-        <ul class="ul-project">
+        <ul class="ul-project" id="project">
             <h2>Project Name: ${this.name}</h2>
             <h3>Project Started: ${this.started}</h3>
             <h3>Project deadline: ${this.deadline}</h3>
@@ -47,5 +48,67 @@ class Projects {
             <input type="submit" data-id=${this.id} class="edit" id="edit" value="Edit Project">
         </ul><br>
         `
+    }
+
+    static editProject() {
+        const projectParentNode = document.getElementById('projects-container')
+        projectParentNode.addEventListener('click', (event) => { //to access each project's edit button
+            console.log('is this working?')
+            console.log(event)
+            const editBtn = document.getElementById('edit')
+            if(editBtn) {
+                console.log('inside edit button')
+                let editProject = document.getElementById("form")
+                editProject.innerHTML += // ERROR: Do not know how to clear form after appending.
+                                              // ERROR: Do not even know if the project object can be updated yet 
+                `
+                <form id='form'>
+                <h2 id="form-header"> Edit project! </h2><br>
+                <label for="name">Project Name:</label>
+                <input type="text" class="name" id="name"><br><br>
+                <label for="started">Project Started:</label>
+                <input type="date" class="started" id="started"><br><br>
+                <label for="deadline">Project Deadline:</label>
+                <input type="date" class="deadline" id="deadline"><br><br>
+                <label for="description">Project Description:</label><br><br>
+                <textarea id="description"></textarea><br><br>
+                <input type="submit" class="submit" data-id=value="Update Project">
+                
+                </form>
+                `
+                editProject.addEventListener("submit", updateProject(event))
+            }
+        })
+    }
+    // callback function for editProject()
+    static updateProject(event) {
+        event.preventDefault()
+        let name = document.getElementById("name").value
+        let started = document.getElementById("started").value
+        let deadline = document.getElementById("deadline").value
+        let description = document.getElementById("description").value
+        let userInput = {
+            name: name, 
+            started: started,
+            deadline: deadline,
+            description: description
+        };
+        fetch('http://localhost:3000/projects', { 
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json',
+                    // 'Accept': 'application/json'
+                },
+                body: JSON.stringify(userInput)
+        })
+            .then(response => response.json())
+            .then(project => {
+                const {id, name, started, deadline, description, developer_id} = project
+               let p = new Projects(id, name, started, deadline, description, developer_id)
+                    p.renderProject()
+            })
+        
+            // let updateProject = document.getElementById("project")
+            // updateProject.reset()
     }
 }
